65ac38eeaa920fc5b6203337e036680bddb1ef53
John Kodumal <john@launchdarkly.com>
2014-12-22 10:12:34 -0800

    Add a license file. Add first cut at event recording.

--- index.js
+++ index.js
@@ -2,27 +2,28 @@ var requestify = require('requestify');
 var sha1 = require('node-sha1');
 var util = require('util');
 
-var defaultConfig = {
-  base_uri : 'https://app.launchdarkly.com'
-};
+var VERSION = "0.0.3";
 
-var VERSION = "0.0.2";
+global.setImmediate = global.setImmediate || process.nextTick.bind(process);
+
 
 var new_client = function(api_key, config) {
   var client = {};
 
-  if (!config) {
-    client.config = defaultConfig;
-  } 
-  else {
-    client.config = config;
-  }
+  config = config || {};
+
+  client.base_uri = config.base_uri || 'https://app.launchdarkly.com';
+  client.connect_timeout = config.connect_timeout || 2;
+  client.read_timeout = config.read_timeout || 10;
+  client.capacity = config.capacity || 1000;
+  client.flush_interval = config.flush_interval || 5;  
+  client.api_key = api_key;
+  client.queue = [];
+
 
   if (!api_key) {
     throw new Error("You must configure the client with an API key");
   }
-  
-  client.api_key = api_key;
 
   requestify.cacheTransporter({
     cache: {},
@@ -42,26 +43,59 @@ var new_client = function(api_key, config) {
 
   client.get_flag = function(key, user, default_val, fn) {
     if (!key || !user) {
+      send_flag_event(client, key, user, default_val);
       fn(default_val);
     }
 
-    requestify.request(client.config.base_uri + '/api/eval/features/' + key, {
+    requestify.request(this.base_uri + '/api/eval/features/' + key, {
       method: "GET",
       headers: {
-        'Authorization': 'api_key ' + client.api_key,
+        'Authorization': 'api_key ' + this.api_key,
         'User-Agent': 'NodeJSClient/' + VERSION
       }
     })
     .then(function(response) {      
       var result = evaluate(response.getBody(), user);
       if (result == null) {
+        send_flag_event(client, key, user, default_val);
         fn(default_val);
       } else {
+        send_flag_event(client, key, user, result);
         fn(result);
       }
     });
   }
 
+  client.track = function(eventName, user, data) {
+    var event = {"key": eventName, 
+                "kind": "custom", 
+                "creationDate": new Date().getTime()};
+
+    if (data) {
+      event.data = data;
+    }
+
+    enqueue(client, event);
+  }
+
+  client.flush = function(fn) {
+    if (!this.queue.length) return setImmediate(fn);
+
+    requestify.request(this.base_uri + '/api/events/bulk', {
+      method: "POST",
+      headers: {
+        'Authorization': 'api_key ' + this.api_key,
+        'User-Agent': 'NodeJSClient/' + VERSION
+      },
+      body: this.queue
+    })
+    .then(function(response) {
+      this.queue = [];
+      fn(response);
+    });
+  }
+
+  setTimeout(client.flush.bind(client), client.flush_interval);
 
   return client;
 };
@@ -69,6 +103,27 @@ var new_client = function(api_key, config) {
 module.exports = {
   init: new_client
 }
+
+function enqueue(client, event) {
+  client.queue.push(event);
+
+  if (client.queue.length >= client.capacity) {
+    client.flush();
+  } 
+}
+
+function send_flag_event(client, key, user, value) {
+  var event = {
+    "kind": "feature",
+    "key": key,
+    "user": user,
+    "value": value,
+    "creationDate": new Date().getTime()
+  }
+
+  enqueue(client, event);
+}
+
 
 function param_for_user(feature, user) {
   var idHash, hashKey, hashVal, result;
@@ -179,9 +234,19 @@ function intersect_safe(a, b)
 
 var main = function(){
   var client = new_client("7f60f21f-0552-4756-ae32-ca65a0c96ca8", {base_uri: "http://localhost:3000"});
-  client.get_flag("engine.enable", {"key": "user@test.com"}, false, function(flag) {
-    console.log(flag);
-  });
+
+  for (i = 0; i < 1000; i++) {
+    (function(i) {
+      client.get_flag("engine.enable", {"key": "user@test.com"}, false, function(flag) {
+      console.log("Iteration " + i + ": " +flag);
+      if (i == 999) {
+        client.flush(function() {
+          console.log("Flushing client");
+        })
+      }
+      })
+    })(i);    
+  }
 }
 
 if (require.main === module) {
