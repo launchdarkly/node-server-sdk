272c9e92f02a3a4bc7049a4de81dfd01d97b222a
John Kodumal <john@launchdarkly.com>
2015-02-05 15:08:03 -0800

    Compute intersections using filter

--- index.js
+++ index.js
@@ -282,20 +282,7 @@ function evaluate(feature, user) {
 
 function intersect_safe(a, b)
 {
-  var ai=0, bi=0;
-  var result = new Array();
-
-  while( ai < a.length && bi < b.length )
-  {
-     if      (a[ai] < b[bi] ){ ai++; }
-     else if (a[ai] > b[bi] ){ bi++; }
-     else /* they're equal */
-     {
-       result.push(a[ai]);
-       ai++;
-       bi++;
-     }
-  }
-
-  return result;
+  return a.filter(function(value) {
+    return b.indexOf(value) > -1;
+  });
 }
