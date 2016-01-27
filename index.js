4e14df92d925d53bc15a7fc93b0c93cd3f72f15a
John Kodumal <john@launchdarkly.com>
2016-01-26 16:22:21 -0800

    Short-circuit callbacks

--- index.js
+++ index.js
@@ -133,16 +133,19 @@ var new_client = function(api_key, config) {
 
     if (this.offline) {
       cb(null, default_val);
+      return;
     }
 
     else if (!key) {
       send_flag_event(client, key, user, default_val);
       cb(new Error("[LaunchDarkly] No flag key specified in toggle call"), default_val);
+      return;
     }
 
     else if (!user) {
       send_flag_event(client, key, user, default_val);
       cb(new Error("[LaunchDarkly] No user specified in toggle call"), default_val);
+      return;
     }
     
     if (this.stream && this.initialized) {
@@ -164,9 +167,11 @@ var new_client = function(api_key, config) {
       if (result === null) {
           send_flag_event(client, key, user, default_val);
           cb(null, default_val);
+          return;
       } else {
         send_flag_event(client, key, user, result);
         cb(null, result);
+        return;
       }        
     }
     else {
@@ -175,13 +180,16 @@ var new_client = function(api_key, config) {
         if (result === null) {
           send_flag_event(client, key, user, default_val);
           cb(null, default_val);
+          return;
         } else {
           send_flag_event(client, key, user, result);
           cb(null, result);
+          return;
         }
       },
       function(error) {
         cb(error, default_val);
+        return;
       });
     }
 
@@ -257,8 +265,10 @@ var new_client = function(api_key, config) {
     })
     .then(function(response) {
       cb(null, response);
+      return;
     }, function(error) {
       cb(error, null);
+      return;
     });
   };
 
