5bed56b958a85cb4f902044531cf05fabc642b8a
John Kodumal <john@launchdarkly.com>
2016-06-22 21:35:45 -0700

    Support for most operators (except before and after)

--- operators.js
+++ operators.js
@@ -1,10 +1,37 @@
 
 var operators = {
   "in": function(a, b) {
-    if (a === b) {
-      return true;
-    }
-    // TODO continue adding operators
+    return a === b;
+  },
+  "endsWith": function(a, b) {
+    return typeof a === 'string' && a.endsWith(b);
+  },
+  "startsWith": function(a, b) {
+    return typeof a === 'string' && a.startsWith(b);
+  },
+  "matches": function(a, b) {
+    return typeof b === 'string' && new RegExp(b).test(a);
+  },
+  "contains": function(a, b) {
+    return typeof a === 'string' && a.indexOf(b) > -1;
+  },
+  "lessThan": function(a, b) {
+    return typeof a === 'number' && a < b;
+  },
+  "lessThanOrEqual": function(a, b) {
+    return typeof a === 'number' && a <= b;
+  },
+  "greaterThan": function(a, b) {
+    return typeof a === 'number' && a > b;
+  },
+  "greaterThanOrEqual": function(a, b) {
+    return typeof a === 'number' && a >= b;
+  },
+  "before": function(a, b) {
+    // TODO
+  },
+  "after": function(a, b) {
+    // TODO
   }
 }
 
