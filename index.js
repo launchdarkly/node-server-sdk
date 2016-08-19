c130dc8faa1590173bc7ce7760a10867846c9f14
John Kodumal <john@launchdarkly.com>
2016-08-19 14:59:03 -0700

    Bump version

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ var tunnel = require('tunnel');
 var winston = require('winston');
 var crypto = require('crypto');
 var async = require('async');
-var VERSION = "3.0.1";
+var VERSION = "3.0.2";
 
 var noop = function(){};
 
