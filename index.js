4610ad6f47d13b9c673af21616c1628387178045
akannalathws3 <akannalath@websystem3.com>
2017-10-03 11:13:57 -0700

    Change error emit string

--- index.js
+++ index.js
@@ -74,7 +74,7 @@ var new_client = function(sdk_key, config) {
         }
         
         config.logger.error("[LaunchDarkly]", error);
-        client.emit('error') // Allow conneciton error handling
+        client.emit('connectError') // Allow conneciton error handling
       }
       else if (!init_complete) {
         init_complete = true;        
