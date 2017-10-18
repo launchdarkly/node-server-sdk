ecdbf85c85a4b3f1b4c134c75066642508ed0ba1
Eli Bishop <this@errorbar.net>
2017-10-17 18:11:05 -0700

    use request-etag for basic caching

--- requestor.js
+++ requestor.js
@@ -1,4 +1,4 @@
-var request = require('request');
+var ETagRequest = require('request-etag');
 /**
  * Creates a new Requestor object, which handles remote requests to fetch feature flags for LaunchDarkly.
  * This is never called synchronously when requesting a feature flag for a user (e.g. via the toggle) call.
@@ -13,23 +13,10 @@ var request = require('request');
 function Requestor(sdk_key, config) {
   var requestor = {};
 
-  // TODO: implement minimal caching via ETags - request doesn't do this for us
-  
-  // requestify.cacheTransporter({
-  //   cache: {},
-  //   get: function(url, fn) {
-  //     fn(null, this.cache[url]);
-  //   },
-
-  //   set: function(url, response, fn) {
-  //     this.cache[url] = response;
-  //     fn();
-  //   },
-  //   purge: function(url, fn) {
-  //     delete this.cache[url];
-  //     fn();
-  //   }
-  // });
+  var cacheConfig = {
+    max: 100
+  };
+  var requestWithETagCaching = new ETagRequest(cacheConfig);
 
   function make_request(resource) {
     var request_params = {
@@ -44,20 +31,26 @@ function Requestor(sdk_key, config) {
     }
 
     return function(cb, err_cb) {
-      request(request_params)
-        .on('response', cb)
-        .on('error', err_cb);
+      requestWithETagCaching(request_params, function(err, resp, body) {
+        // Note that when request-etag gives us a cached response, the body will only be in the "body"
+        // callback parameter -- not in resp.getBody().  For a fresh response, it'll be in both.
+        if (err) {
+          err_cb(err);
+        } else {
+          cb(resp, body);
+        }
+      });
     };
   }
 
   requestor.request_flag = function(key, cb) {
     var req = make_request('/sdk/latest-flags/' + key);
     req(
-      function(response) {
+      function(response, body) {
         if (response.code !== 200) {
           cb(new Error('Unexpected status code: ' + response.code), null);
         } else {
-          cb(null, response.getBody());
+          cb(null, body);
         }
       },
       function(err) {
@@ -69,11 +62,11 @@ function Requestor(sdk_key, config) {
   requestor.request_all_flags = function(cb) {
     var req = make_request('/sdk/latest-flags');
     req(
-      function(response) {
+      function(response, body) {
         if (response.code !== 200) {
           cb(new Error('Unexpected status code: ' + response.code), null);
         } else {
-          cb(null, response.getBody());
+          cb(null, body);
         }
       },
       function(err) {
