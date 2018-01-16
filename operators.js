f9013fe17283495353ad46e0c882f0ad8ce4bdc6
Eli Bishop <eli@launchdarkly.com>
2018-01-16 15:00:53 -0800

    add semantic version operators

--- operators.js
+++ operators.js
@@ -1,4 +1,35 @@
 
+var semver = require('semver');
+
+function semVerOperator(fn) {
+  return function(a, b) {
+    var av = parseSemVer(a), bv = parseSemVer(b);
+    return (av && bv) ? fn(av, bv) : false;
+  };
+}
+
+function parseSemVer(input) {
+  var ret = semver.parse(input);
+  if (!ret) {
+    input = addZeroVersionComponent(input);
+    ret = semver.parse(input);
+    if (!ret) {
+      input = addZeroVersionComponent(input);
+      ret = semver.parse(input);
+    }
+  }
+  return ret;
+}
+
+function addZeroVersionComponent(input) {
+  // allows for loose versions like "2" or "2.0-rc1"
+  var matches = new RegExp("^([0-9.]*)(.*)").exec(input);
+  if (!matches) {
+    return input + ".0";
+  }
+  return matches[1] + ".0" + matches[2];
+}
+
 var operators = {
   "in": function(a, b) {
     return a === b;
@@ -52,8 +83,11 @@ var operators = {
       return a > b;
     }
     return false;
-  }
-}
+  },
+  "semVerEqual": semVerOperator(function(a, b) { return a.compare(b) == 0; }),
+  "semVerLessThan": semVerOperator(function(a, b) { return a.compare(b) < 0; }),
+  "semVerGreaterThan": semVerOperator(function(a, b) { return a.compare(b) > 0; })
+};
 
 var operator_none = function(a, b) {
   return false;
