6d77ea2bd9a874c1db1026841915695db756de4f
Eli Bishop <eli@launchdarkly.com>
2019-02-07 11:01:47 -0800

    fix config file so we don't need a custom theme

--- docs/typedoc.js
+++ docs/typedoc.js
@@ -1,13 +1,17 @@
 
+// Note that the format of this file is barely documented on the TypeDoc site. In general,
+// the properties are equivalent to the command-line options described here:
+// https://typedoc.org/api/
+
 module.exports = {
   out: './docs/build/html',
-  includes: '.',
   exclude: [
     '**/node_modules/**',
     'test-types.ts'
   ],
   name: 'LaunchDarkly Node SDK',
-  theme: 'docs/theme',
-  readme: 'none',
-  includeDeclarations: true
+  readme: 'none',                // don't add a home page with a copy of README.md
+  mode: 'file',                  // don't treat "index.d.ts" itself as a parent module
+  includeDeclarations: true,     // allows it to process a .d.ts file instead of actual TS code
+  entryPoint: '"ldclient-node"'  // note extra quotes - workaround for a TypeDoc bug
 };
