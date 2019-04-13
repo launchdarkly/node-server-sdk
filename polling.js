49422ac7dc1ed20b3b31d29ecfeb3068df6e6d34
Eli Bishop <eli@launchdarkly.com>
2019-04-12 17:39:33 -0700

    misc fixes in polling

--- polling.js
+++ polling.js
@@ -19,11 +19,12 @@ function PollingProcessor(config, requestor) {
     startTime = new Date().getTime();
     config.logger.debug("Polling LaunchDarkly for feature flag updates");
     requestor.requestAllData(function(err, resp) {
-      elapsed = new Date().getTime() - startTime;
-      sleepFor = Math.max(config.pollInterval * 1000 - elapsed, 0);
+      const elapsed = new Date().getTime() - startTime;
+      const sleepFor = Math.max(config.pollInterval * 1000 - elapsed, 0);
       config.logger.debug("Elapsed: %d ms, sleeping for %d ms", elapsed, sleepFor);
       if (err) {
-        cb(new errors.LDPollingError(messages.httpErrorMessage(err.status, 'polling request', 'will retry')));
+        const message = err.status || err.message;
+        cb(new errors.LDPollingError(messages.httpErrorMessage(message, 'polling request', 'will retry')));
         if (!errors.isHttpErrorRecoverable(err.status)) {
           config.logger.error('Received 401 error, no further polling requests will be made since SDK key is invalid');
         } else {
