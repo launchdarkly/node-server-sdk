1d2135648494ff61ba02242f40d7dcee4bb1dab5
Eli Bishop <eli@launchdarkly.com>
2019-10-25 11:08:43 -0700

    add TS syntax check code

--- test-types.ts
+++ test-types.ts
@@ -1,8 +1,11 @@
 
 // This file exists only so that we can run the TypeScript compiler in the CI build
-// to validate our index.d.ts file.
+// to validate our index.d.ts file. This code will not actually be run - the point is
+// just to verify that the type declarations exist and are correct so a TypeScript
+// developer can use all of the SDK features.
 
 import * as ld from 'launchdarkly-node-server-sdk';
+import * as redis from 'redis';
 
 var logger: ld.LDLogger = {
   error: (...args) => { },
@@ -80,3 +83,10 @@ client.variation('key', user, 2).then((value: ld.LDFlagValue) => { });
 client.variation('key', user, 'default').then((value: ld.LDFlagValue) => { });
 client.variationDetail('key', user, 'default').then((detail: ld.LDEvaluationDetail) => { });
 client.allFlags(user).then((flagSet: ld.LDFlagSet) => { });
+
+// Redis integration
+var redisStore0 = ld.RedisFeatureStore();
+var myRedisOpts: redis.ClientOpts = {};
+var redisStore1 = ld.RedisFeatureStore(myRedisOpts, 30, 'prefix', logger);
+var myRedisClient: redis.RedisClient = new redis.RedisClient(myRedisOpts);
+var redisStore2 = ld.RedisFeatureStore(undefined, 30, 'prefix', logger, myRedisClient);
