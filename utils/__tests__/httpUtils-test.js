43db5afd46e886d0b1b36e5c99e09a0cdc9c16a7
Eli Bishop <eli@launchdarkly.com>
2019-11-18 17:21:01 -0800

    fix tests

--- utils/__tests__/httpUtils-test.js
+++ utils/__tests__/httpUtils-test.js
@@ -3,25 +3,25 @@ const packageJson = require('../../package.json');
 
 it('sets SDK key', () => {
   const h = httpUtils.getDefaultHeaders('my-sdk-key', {});
-  expect(h).toMatchObject({ Authorization: 'my-sdk-key' });
+  expect(h).toMatchObject({ authorization: 'my-sdk-key' });
 });
 
 it('sets user agent', () => {
   const h = httpUtils.getDefaultHeaders('my-sdk-key', {});
-  expect(h).toMatchObject({ 'User-Agent': 'NodeJSClient/' + packageJson.version });
+  expect(h).toMatchObject({ 'user-agent': 'NodeJSClient/' + packageJson.version });
 });
 
 it('does not include wrapper header by default', () => {
   const h = httpUtils.getDefaultHeaders('my-sdk-key', {});
-  expect(h['X-LaunchDarkly-Wrapper']).toBeUndefined();
+  expect(h['x-launchdarkly-wrapper']).toBeUndefined();
 });
 
 it('sets wrapper header with name only', () => {
   const h = httpUtils.getDefaultHeaders('my-sdk-key', { wrapperName: 'my-wrapper' });
-  expect(h).toMatchObject({ 'X-LaunchDarkly-Wrapper': 'my-wrapper' });
+  expect(h).toMatchObject({ 'x-launchdarkly-wrapper': 'my-wrapper' });
 });
 
 it('sets wrapper header with name and version', () => {
   const h = httpUtils.getDefaultHeaders('my-sdk-key', { wrapperName: 'my-wrapper', wrapperVersion: '2.0' });
-  expect(h).toMatchObject({ 'X-LaunchDarkly-Wrapper': 'my-wrapper/2.0' });
+  expect(h).toMatchObject({ 'x-launchdarkly-wrapper': 'my-wrapper/2.0' });
 });
